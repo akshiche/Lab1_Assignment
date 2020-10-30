a055a8595c9acc4d08be267e4549c662ead605c0
akshiche <70131706+akshiche@users.noreply.github.com>
2020-10-30 09:38:33 +0530

    lab3 assignment

---
+++ lab3 assignment/MirrorImage.java
@@ -0,0 +1,19 @@
+/**
+Author: Akshitha
+Desc  : To return the string with its mirror image
+*/
+import java.util.*;
+public class MirrorImage {
+
+	public static void main(String[] args) {		
+		// taking input using StringBuffer
+		StringBuffer givenString=new StringBuffer("Earth"); 		
+		System.out.println(givenString+"|"+getImage(givenString));
+	}
+//method to provide the mirror image
+	private static StringBuffer getImage(StringBuffer givenString) {
+		StringBuffer reversed1= givenString.reverse();
+		return reversed1;
+	}
+
+}
